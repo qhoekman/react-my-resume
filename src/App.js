77f7de324b4532af70aeddc77302f846b813331a
Quido <quido@qhoekman.nl>
2019-02-20 08:17:23 +0100

    First draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
+import { PDFViewer } from '@react-pdf/renderer';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { MyDocument } from './elements/Document';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <PDFViewer style={{ width: '100vw', height: '100vh' }}>
+        <MyDocument />
+      </PDFViewer>
     );
   }
 }
